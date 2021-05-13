31432df5408801592283e32272fbbb738aa4b212
Brasseur Thibault <t.brasseur@activigie.com>
2021-05-13 13:28:15 +0200

    Second commit : Install json-Server => json-server/   ( npm install -g json-server + json-server --watch db.json (https://www.npmjs.com/package/json-server) )

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,18 @@
+import React, { useEffect } from "react";
 import './App.css';
+import api from "./api/db";
 
 function App() {
+
+  useEffect(() => {
+      const getDb = async () => {
+        const response = await api.get("/residents");
+        console.log("=== DATA =>", response.data)
+        return response.data;
+      };
+    getDb();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,5 +21,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
